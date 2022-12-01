e08edc3fa2106cfaa28086a1678e5373e126bc6f
Andrew Courter <exosyphon@yahoo.com>
2022-11-30 21:47:22 -0600

    Initial commit for day 1

---
+++ day_1/src/test/kotlin/Day1Test.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class Day1 {
+    @Test
+    fun `first test`() {
+        assertEquals(expected = true, actual = true)
+    }
+}
