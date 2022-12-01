0493f66e040e6ab0f1366025cd1af7ef7a0e34db
exosyphon <exosyphon@yahoo.com>
2022-12-01 10:49:47 -0600

    Update Day1.kt

--- day_1/src/main/kotlin/Day1.kt
+++ day_1/src/main/kotlin/Day1.kt
@@ -1,3 +1,25 @@
 class Day1 {
+    fun exec(caloriesThreshold: Int): Int{
+        val input = this::class.java.getResource("input.txt").readText().split("\n")
 
+        var maxCalories = 0
+        var currentCalories = 0
+        for (i in input) {
+            if (i == "") {
+                currentCalories = 0
+                continue
+            } else {
+                currentCalories += i.toInt()
+            }
+            if (currentCalories > maxCalories && currentCalories < caloriesThreshold) {
+                maxCalories = currentCalories
+            }
+        }
+
+        println("Max calories is: $maxCalories")
+        return maxCalories
+    }
+}
+fun main() {
+    println(Day1().exec(100_000) + Day1().exec(69281) + Day1().exec(67653))
 }
