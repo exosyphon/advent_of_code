64c814eda5b44d179e3112e4e1fc23a7abca5d10
Andrew Courter <exosyphon@yahoo.com>
2022-12-01 23:52:33 -0600

    Day 2 part 1

---
+++ day_2/src/main/kotlin/Day2.kt
@@ -0,0 +1,51 @@
+class Day2 {
+    fun exec(): Int {
+        val input = this::class.java.getResource("input.txt").readText()
+
+        val win = 6
+        val loss = 0
+        val tie = 3
+
+        val values = mapOf(
+            "X" to 1,
+            "Y" to 2,
+            "Z" to 3
+        )
+
+        // A rock
+        // B paper
+        // C scissors
+        // X rock
+        // Y paper
+        // Z scissors
+
+        return input.split("\n").mapNotNull { outer ->
+            val items = outer.split(" ")
+            if (items[0] == "A" && items[1] == "Y") {
+                win + values[items[1]]!!
+            } else if (items[0] == "A" && items[1] == "Z") {
+                loss + values[items[1]]!!
+            } else if (items[0] == "A" && items[1] == "X") {
+                tie + values[items[1]]!!
+            } else if (items[0] == "B" && items[1] == "X") {
+                loss + values[items[1]]!!
+            } else if (items[0] == "B" && items[1] == "Y") {
+                tie + values[items[1]]!!
+            } else if (items[0] == "B" && items[1] == "Z") {
+                win + values[items[1]]!!
+            } else if (items[0] == "C" && items[1] == "X") {
+                win + values[items[1]]!!
+            } else if (items[0] == "C" && items[1] == "Y") {
+                loss + values[items[1]]!!
+            } else if (items[0] == "C" && items[1] == "Z") {
+                tie + values[items[1]]!!
+            } else {
+                0
+            }
+        }.sum()
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Day2().exec())
+}
---
+++ day_2/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day2Test {
+
+    @Test
+    fun `returns strategy total score`() {
+        assertEquals(11666, Day2().exec())
+    }
+
+    @Test
+    fun `returns `() {
+        assertEquals(, Day2().exec())
+    }
+
+}
