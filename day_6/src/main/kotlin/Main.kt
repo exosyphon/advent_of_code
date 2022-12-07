201b85ded088993f7af22261cc03cffb95230da1
Andrew Courter <exosyphon@yahoo.com>
2022-12-07 01:46:22 -0600

    Day 6 Part 2

--- day_6/src/main/kotlin/Main.kt
+++ day_6/src/main/kotlin/Main.kt
@@ -18,8 +18,29 @@ class Main {
 
         return marker
     }
+
+    fun part2(): Int {
+        val input = this::class.java.getResource("input.txt").readText()
+
+        var marker = 0
+        var tracker = ArrayDeque<Char>(14)
+        for ((index, character) in input.withIndex()) {
+            if (index > 13) {
+                if (tracker.distinct().size == 14) {
+                    marker = index
+                    break
+                }
+                tracker.removeFirst()
+            }
+
+            tracker.addLast(character)
+        }
+
+        return marker
+    }
 }
 
 fun main() {
     println(Main().part1())
+    println(Main().part2())
 }
