361851fbd76ae7a7795e290e435fadd246b131e7
Andrew Courter <exosyphon@yahoo.com>
2022-12-11 23:15:26 -0600

    Day 8 Part 2

--- day_8/src/main/kotlin/Main.kt
+++ day_8/src/main/kotlin/Main.kt
@@ -4,18 +4,38 @@ class Main {
     fun part1(): Int {
         val input = this::class.java.getResource("input.txt").readText().trim().split("\n")
 
-        for ((rowIndex, row) in input.withIndex()) {
-            if (rowIndex != 0) {
-                forest.add(mutableListOf())
-            }
-            for ((colIndex, column) in row.toCharArray().withIndex()) {
-                if (colIndex == 0) {
-                    forest[rowIndex] = mutableListOf()
-                }
-                forest[rowIndex].add(Tree(column.digitToInt(), true))
-            }
-        }
+        buildForest(input)
+        setVisibility()
 
+        return countVisibleTrees()
+    }
+
+    fun part2(): Int {
+        val input = this::class.java.getResource("input.txt").readText().trim().split("\n")
+
+        buildForest(input)
+        setDistance()
+
+        return findMax()
+    }
+
+    private fun findMax(): Int {
+        var max = 0
+        var i = 0
+        while (i < forest.size) {
+            var j = 0
+            while (j < forest[i].size) {
+                if (forest[i][j].totalDistance > max) {
+                    max = forest[i][j].totalDistance
+                }
+                j++
+            }
+            i++
+        }
+        return max
+    }
+
+    private fun setVisibility() {
         var i = 1
         while (i < forest.size - 1) {
             var j = 1
@@ -34,11 +54,88 @@ class Main {
             }
             i++
         }
-        for (f in forest) {
-            println(f)
-        }
+    }
 
-        return countVisibleTrees()
+    private fun setDistance() {
+        var i = 1
+        while (i < forest.size - 1) {
+            var j = 1
+            while (j < forest[i].size - 1) {
+                val leftDistance = checkLeftRowDistance(i, j, forest[i][j])
+                val rightDistance = checkRightRowDistance(i, j, forest[i][j])
+                val topDistance = checkTopColDistance(i, j, forest[i][j])
+                val bottomDistance = checkBottomColDistance(i, j, forest[i][j])
+                forest[i][j].totalDistance = leftDistance * rightDistance * topDistance * bottomDistance
+                j++
+            }
+            i++
+        }
+    }
+
+    private fun buildForest(input: List<String>) {
+        for ((rowIndex, row) in input.withIndex()) {
+            if (rowIndex != 0) {
+                forest.add(mutableListOf())
+            }
+            for ((colIndex, column) in row.toCharArray().withIndex()) {
+                if (colIndex == 0) {
+                    forest[rowIndex] = mutableListOf()
+                }
+                forest[rowIndex].add(Tree(column.digitToInt(), true, 0))
+            }
+        }
+    }
+
+    private fun checkRightRowDistance(row: Int, col: Int, tree: Tree): Int {
+        var result = 0
+        var j = col + 1
+        while (j < forest[row].size) {
+            result += 1
+            if (forest[row][j].height >= tree.height) {
+                break
+            }
+            j++
+        }
+        return result
+    }
+
+    private fun checkLeftRowDistance(row: Int, col: Int, tree: Tree): Int {
+        var result = 0
+        var j = col - 1
+        while (j >= 0) {
+            result += 1
+            if (forest[row][j].height >= tree.height) {
+                break
+            }
+            j--
+        }
+        return result
+    }
+
+    private fun checkBottomColDistance(row: Int, col: Int, tree: Tree): Int {
+        var result = 0
+        var j = row + 1
+        while (j < forest.size) {
+            result += 1
+            if (forest[j][col].height >= tree.height) {
+                break
+            }
+            j++
+        }
+        return result
+    }
+
+    private fun checkTopColDistance(row: Int, col: Int, tree: Tree): Int {
+        var result = 0
+        var j = row - 1
+        while (j >= 0) {
+            result += 1
+            if (forest[j][col].height >= tree.height) {
+                break
+            }
+            j--
+        }
+        return result
     }
 
     private fun checkLeftRowVisibility(row: Int, col: Int, tree: Tree): Boolean {
@@ -122,8 +219,9 @@ class Main {
     }
 }
 
-data class Tree(val height: Int, var visible: Boolean)
+data class Tree(val height: Int, var visible: Boolean, var totalDistance: Int)
 
 fun main() {
     println(Main().part1())
+    println(Main().part2())
 }
