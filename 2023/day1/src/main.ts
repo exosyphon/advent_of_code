1ca32fd2822fa6502572658d7c351d0c9ca536aa
Andrew Courter <exosyphon@yahoo.com>
2023-12-02 13:33:47 -0600

    Add Day1 part2

--- 2023/day1/src/main.ts
+++ 2023/day1/src/main.ts
@@ -1,17 +1,76 @@
 import fs from "fs"
 
 export class Main {
-  public calibrations(): number {
+  public calibrations(part2 = false): number {
     let sum = 0
 
     const allFileContents = fs.readFileSync("./src/input.txt", "utf-8")
     allFileContents.split(/\r?\n/).forEach((line: string) => {
       if (line.length > 0) {
-        sum = sum + this.calibration(line)
+        if (part2) {
+          sum = sum + this.improvedCalibrations(line)
+        } else {
+          sum = sum + this.calibration(line)
+        }
       }
     })
 
     return sum
+  }
+
+  public improvedCalibrations(input: string): number {
+    const numbers: { [key: string]: number } = {
+      "1": 1,
+      "2": 2,
+      "3": 3,
+      "4": 4,
+      "5": 5,
+      "6": 6,
+      "7": 7,
+      "8": 8,
+      "9": 9,
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+    }
+
+    const indexes = Object.entries(numbers).flatMap(([key, value]) => {
+      return this.getAllIndexes(input, key).map((index) => {
+        return { index: index, value: value }
+      })
+    }).filter((entry) => entry.index != -1)
+
+    const maxObj = indexes.reduce((mObj, obj) => {
+      return obj.index > mObj.index ? obj : mObj
+    },
+      { index: -Infinity },
+    )
+
+    const minObj = indexes.reduce((mObj, obj) => {
+      return obj.index < mObj.index ? obj : mObj
+    },
+      { index: Infinity },
+    )
+
+    return Number(`${minObj.value}${maxObj.value}`)
+  }
+
+  private getAllIndexes(mainString: string, subString: string): number[] {
+    const indexes: number[] = []
+    let currentIndex = mainString.indexOf(subString)
+
+    while (currentIndex !== -1) {
+      indexes.push(currentIndex)
+      currentIndex = mainString.indexOf(subString, currentIndex + 1)
+    }
+
+    return indexes
   }
 
   public calibration(input: string): number {
@@ -19,5 +78,3 @@ export class Main {
     return Number(`${onlyNumbers[0]}${onlyNumbers[onlyNumbers.length - 1]}`)
   }
 }
-
-console.log(new Main().calibrations())
--- 2023/day1/test/main.test.ts
+++ 2023/day1/test/main.test.ts
@@ -1,18 +1,84 @@
-import { expect, test } from "bun:test"
+import { describe, expect, test } from "bun:test"
 import { Main } from "../src/main"
 
-test("first case", () => {
-  expect(new Main().calibration("1abc2")).toBe(12)
-})
-test("second case", () => {
-  expect(new Main().calibration("pqr3stu8vwx")).toBe(38)
-})
-test("third case", () => {
-  expect(new Main().calibration("a1b2c3d4e5f")).toBe(15)
-})
-test("fourth case", () => {
-  expect(new Main().calibration("treb7uchet")).toBe(77)
-})
-test("test file case", () => {
-  expect(new Main().calibrations()).toBe(52974)
+describe("Main", () => {
+  test("first case", () => {
+    expect(new Main().calibration("1abc2")).toBe(12)
+  })
+  test("second case", () => {
+    expect(new Main().calibration("pqr3stu8vwx")).toBe(38)
+  })
+  test("third case", () => {
+    expect(new Main().calibration("a1b2c3d4e5f")).toBe(15)
+  })
+  test("fourth case", () => {
+    expect(new Main().calibration("treb7uchet")).toBe(77)
+  })
+  test("test file case", () => {
+    expect(new Main().calibrations()).toBe(52974)
+  })
+
+  test("part 2 first case", () => {
+    expect(new Main().improvedCalibrations("two1nine")).toBe(29)
+  })
+  test("part 2 second case", () => {
+    expect(new Main().improvedCalibrations("eightwothree")).toBe(83)
+  })
+  test("part 2 third case", () => {
+    expect(new Main().improvedCalibrations("abcone2threexyz")).toBe(13)
+  })
+  test("part 2 fourth case", () => {
+    expect(new Main().improvedCalibrations("xtwone3four")).toBe(24)
+  })
+  test("part 2 fifth case", () => {
+    expect(new Main().improvedCalibrations("4nineeightseven2")).toBe(42)
+  })
+  test("part 2 sixth case", () => {
+    expect(new Main().improvedCalibrations("zoneight234")).toBe(14)
+  })
+  test("part 2 seventh case", () => {
+    expect(new Main().improvedCalibrations("7pqrstsixteen")).toBe(76)
+  })
+  test("part 2 special case 1", () => {
+    expect(new Main().improvedCalibrations("eighthree")).toBe(83)
+  })
+  test("part 2 special case 2", () => {
+    expect(new Main().improvedCalibrations("oneight")).toBe(18)
+  })
+  test("part 2 special case 3", () => {
+    expect(new Main().improvedCalibrations("abc2x3oneight")).toBe(28)
+  })
+  test("part 2 special case 4", () => {
+    expect(new Main().improvedCalibrations("f3")).toBe(33)
+  })
+  test("part 2 special case 5", () => {
+    expect(new Main().improvedCalibrations("two934seven1")).toBe(21)
+  })
+  test("part 2 special case 6", () => {
+    expect(new Main().improvedCalibrations("8825eightknfv")).toBe(88)
+  })
+  test("part 2 special case 7", () => {
+    expect(new Main().improvedCalibrations("sevenoneqbfzntsix55")).toBe(75)
+  })
+  test("part 2 special case 8", () => {
+    expect(new Main().improvedCalibrations("4twofour")).toBe(44)
+  })
+  test("part 2 special case 9", () => {
+    expect(new Main().improvedCalibrations("573")).toBe(53)
+  })
+  test("part 2 special case 10", () => {
+    expect(new Main().improvedCalibrations("5n")).toBe(55)
+  })
+  test("part 2 special case 11", () => {
+    expect(new Main().improvedCalibrations("265one")).toBe(21)
+  })
+  test("part 2 special case 12", () => {
+    expect(new Main().improvedCalibrations("seven3lbcvjxqhhdpzkttqsixjzzjjbclfq1fiveeightwojx")).toBe(72)
+  })
+  test("part 2 special case 13", () => {
+    expect(new Main().improvedCalibrations("38sevennineninemnfzklttkxnine3")).toBe(33)
+  })
+  test("part 2 full case", () => {
+    expect(new Main().calibrations(true)).toBe(53340)
+  })
 })
