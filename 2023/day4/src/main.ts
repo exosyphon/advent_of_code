d07e61e0aefc980e696fa376e9b1acc915af56d6
exosyphon <exosyphon@yahoo.com>
2023-12-04 14:24:22 -0600

    Update main.ts for day4 part1 2023

--- 2023/day4/src/main.ts
+++ 2023/day4/src/main.ts
@@ -3,10 +3,24 @@ import fs from "fs"
 export class Main {
   public execute(part2 = false): number {
     const allFileContents = fs.readFileSync("./src/input.txt", "utf-8")
-    // allFileContents.split(/\r?\n/).forEach((line: string) => {
-    // })
+    // return allFileContents.split(/\r?\n/).reduce((acc: number, line: string) => {
+    //   return acc + this.processCard(line)
+    // }, 0)
 
-    return 0
+    let sum = 0
+    allFileContents.split("\n").forEach((line: string) => {
+      if (line.length > 0) {
+        sum = sum + this.processCard(line)
+      }
+    })
+    return sum
   }
 
+  public processCard(input: string): number {
+    const [setA, setB] = input.split(/:\s+/)[1].split(/\|\s+/).map((numbers: string) => {
+      return numbers.split(/\s+/)
+    })
+
+    return Math.floor(Math.pow(2, setB.filter(value => setA.includes(value)).length - 1))
+  }
 }
