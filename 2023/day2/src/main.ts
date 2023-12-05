8d40b1633cf26b1bccd308f3dcb7635dd6fd6fbd
Andrew Courter <exosyphon@yahoo.com>
2023-12-04 22:44:19 -0600

    Add day2 solution

--- 2023/day2/src/main.ts
+++ 2023/day2/src/main.ts
@@ -1,12 +1,63 @@
 import fs from "fs"
 
+type Game = {
+  red: number
+  green: number
+  blue: number
+}
+
 export class Main {
   public execute(part2 = false): number {
     const allFileContents = fs.readFileSync("./src/input.txt", "utf-8")
-    // allFileContents.split(/\r?\n/).forEach((line: string) => {
-    // })
-
-    return 0
+    return allFileContents.split("\n").reduce((acc: number, line: string, index: number) => {
+      if (line.length > 0) {
+        if (part2) {
+          return acc + this.minPossible(line)
+        } else {
+          return acc + (this.isPossible(line) ? index + 1 : 0)
+        }
+      } else {
+        return acc
+      }
+    }, 0)
   }
 
+  public minPossible(input: string): number {
+    const games = input.split(/:\s+/)[1].split(/;\s+/)
+    const gameResults: Game[] = games.map((game: string) => {
+      return game.split(/,\s+/).reduce((acc: Game, cubes: string) => {
+        const cubesObj = cubes.split(" ")
+        acc[cubesObj[1]] = Number(cubesObj[0])
+        return acc
+      }, {})
+    })
+
+    const minCubes: Game = { red: -Infinity, green: -Infinity, blue: -Infinity }
+    gameResults.forEach((gameResult) => {
+      if (gameResult.red > minCubes.red) {
+        minCubes.red = gameResult.red
+      }
+      if (gameResult.blue > minCubes.blue) {
+        minCubes.blue = gameResult.blue
+      }
+      if (gameResult.green > minCubes.green) {
+        minCubes.green = gameResult.green
+      }
+    })
+
+    return minCubes.red * minCubes.green * minCubes.blue
+  }
+
+  public isPossible(input: string): boolean {
+    const games = input.split(/:\s+/)[1].split(/;\s+/)
+    const gameResults: Game[] = games.map((game: string) => {
+      return game.split(/,\s+/).reduce((acc: Game, cubes: string) => {
+        const cubesObj = cubes.split(" ")
+        acc[cubesObj[1]] = Number(cubesObj[0])
+        return acc
+      }, {})
+    })
+
+    return gameResults.filter((gameResult) => gameResult.red > 12 || gameResult.green > 13 || gameResult.blue > 14).length == 0
+  }
 }
--- 2023/day2/test/main.test.ts
+++ 2023/day2/test/main.test.ts
@@ -1,8 +1,43 @@
 import { describe, expect, test } from "bun:test"
 import { Main } from "../src/main"
 
+// 12 red cubes, 13 green cubes, and 14 blue cubes
 describe("Main", () => {
-  test("first case", () => {
-    expect(new Main().calibration("1abc2")).toBe(12)
+  test("part 1 first case", () => {
+    expect(new Main().isPossible("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")).toBe(true)
+  })
+  test("part 1 second case", () => {
+    expect(new Main().isPossible("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")).toBe(true)
+  })
+  test("part 1 third case", () => {
+    expect(new Main().isPossible("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")).toBe(false)
+  })
+  test("part 1 fourth case", () => {
+    expect(new Main().isPossible("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")).toBe(false)
+  })
+  test("part 1 fifth case", () => {
+    expect(new Main().isPossible("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")).toBe(true)
+  })
+  test("part 1 case", () => {
+    expect(new Main().execute()).toBe(2076)
+  })
+
+  test("part 2 first case", () => {
+    expect(new Main().minPossible("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")).toBe(48)
+  })
+  test("part 2 second case", () => {
+    expect(new Main().minPossible("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")).toBe(12)
+  })
+  test("part 2 third case", () => {
+    expect(new Main().minPossible("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")).toBe(1560)
+  })
+  test("part 2 fourth case", () => {
+    expect(new Main().minPossible("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")).toBe(630)
+  })
+  test("part 2 fifth case", () => {
+    expect(new Main().minPossible("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")).toBe(36)
+  })
+  test("part 2 case", () => {
+    expect(new Main().execute(true)).toBe(70950)
   })
 })
