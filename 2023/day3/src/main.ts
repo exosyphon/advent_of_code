987aca2bdf6c26018aa70d05351b169b5acc5549
Andrew Courter <exosyphon@yahoo.com>
2023-12-13 22:21:58 -0600

    Add day 3 part 2 solution

--- 2023/day3/src/main.ts
+++ 2023/day3/src/main.ts
@@ -1,4 +1,4 @@
-import fs from "fs"
+import fs, { watch } from "fs"
 
 type Position = {
   x: number
@@ -19,34 +19,162 @@ export class Main {
     return arr
   }
 
+  private getNumber(matrix: string[][], position: Position): number {
+    //console.log("scanning")
+    let result = ""
+    for (let j = position.y; j >= 0; j--) {
+      //console.log(`checking ${matrix[position.x][j]}`)
+      if (matrix[position.x][j].match(/\d/)) {
+        //console.log(`result before: ${result}`)
+        result = `${matrix[position.x][j]}${result}`
+        //console.log(`result before: ${result}`)
+      } else {
+        break
+      }
+    }
+    for (let j = position.y + 1; j < matrix[position.x].length; j++) {
+      //console.log(`checking ${matrix[position.x][j]}`)
+      if (matrix[position.x][j].match(/\d/)) {
+        //console.log(`result before: ${result}`)
+        result = `${result}${matrix[position.x][j]}`
+        //console.log(`result before: ${result}`)
+      } else {
+        break
+      }
+    }
+    return Number(result)
+  }
+
+  private getNumbers(matrix: string[][], position: Position): number[] {
+    let numbers: number[] = []
+    const symbolRegex = /\d/
+    if (
+      position.x + 1 < matrix.length &&
+      position.y + 1 < matrix[position.x + 1].length &&
+      matrix[position.x + 1][position.y + 1].match(symbolRegex)
+    ) {
+      //console.log("adj1")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x + 1, y: position.y + 1 }),
+      )
+    }
+    if (
+      position.x + 1 < matrix.length &&
+      position.y < matrix[position.x + 1].length &&
+      matrix[position.x + 1][position.y].match(symbolRegex)
+    ) {
+      //console.log("adj2")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x + 1, y: position.y }),
+      )
+    }
+    if (
+      position.x + 1 < matrix.length &&
+      position.y - 1 >= 0 &&
+      matrix[position.x + 1][position.y - 1].match(symbolRegex)
+    ) {
+      //console.log("adj3")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x + 1, y: position.y - 1 }),
+      )
+    }
+    if (
+      position.x < matrix.length &&
+      position.y + 1 < matrix[position.x].length &&
+      matrix[position.x][position.y + 1].match(symbolRegex)
+    ) {
+      //console.log("adj4")
+      numbers = numbers.concat(this.getNumber(matrix, position))
+    }
+    if (
+      position.x < matrix.length &&
+      position.y - 1 >= 0 &&
+      matrix[position.x][position.y - 1].match(symbolRegex)
+    ) {
+      //console.log("adj5")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x, y: position.y - 1 }),
+      )
+    }
+    //console.log(`why no adj6? ${matrix[position.x - 1][position.y - 1]}`)
+    //console.log(`why no adj6 bro? ${matrix[position.x - 1][position.y - 1].match(symbolRegex)}`)
+    if (
+      position.x - 1 >= 0 &&
+      position.y - 1 >= 0 &&
+      matrix[position.x - 1][position.y - 1].match(symbolRegex)
+    ) {
+      //console.log("adj6")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x - 1, y: position.y - 1 }),
+      )
+    }
+    if (
+      position.x - 1 >= 0 &&
+      position.y < matrix[position.x - 1].length &&
+      matrix[position.x - 1][position.y].match(symbolRegex)
+    ) {
+      //console.log("adj7")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x - 1, y: position.y }),
+      )
+    }
+    if (
+      position.x - 1 >= 0 &&
+      position.y + 1 < matrix[position.x - 1].length &&
+      matrix[position.x - 1][position.y + 1].match(symbolRegex)
+    ) {
+      //console.log("adj8")
+      numbers = numbers.concat(
+        this.getNumber(matrix, { x: position.x - 1, y: position.y + 1 }),
+      )
+    }
+    const temp = new Set(numbers)
+    return [...temp]
+  }
+
   public execute({ test = false, part2 = false } = {}): number {
     const arr = this.readInput(test)
-    // arr.forEach((v) => console.log(...v))
+    // arr.forEach((v) => //console.log(...v))
 
     let sum = 0
-
-    for (let i = 0; i < arr.length; i++) {
-      let value = ""
-      let adjacentSymbol = false
-      for (let j = 0; j < arr[i].length; j++) {
-        if (arr[i][j].match(/\d/)) {
-          if (this.checkAdjacent(arr, { x: i, y: j })) {
-            adjacentSymbol = true
-          }
-          value = value.concat(arr[i][j])
-
-          if (j == arr[i].length - 1) {
-            if (adjacentSymbol) {
-              sum = sum + Number(value)
+    if (part2) {
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+          if (arr[i][j].match(/\*/)) {
+            //console.log("found a *")
+            const numbers = this.getNumbers(arr, { x: i, y: j })
+            //console.log(`got these numbers ${numbers}`)
+            if (numbers.length == 2) {
+              //console.log(numbers)
+              sum += numbers[0] * numbers[1]
             }
           }
-        } else {
-          if (value.length > 0) {
-            if (adjacentSymbol) {
-              sum = sum + Number(value)
+        }
+      }
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let value = ""
+        let adjacentSymbol = false
+        for (let j = 0; j < arr[i].length; j++) {
+          if (arr[i][j].match(/\d/)) {
+            if (this.checkAdjacent(arr, { x: i, y: j })) {
+              adjacentSymbol = true
             }
-            value = ""
-            adjacentSymbol = false
+            value = value.concat(arr[i][j])
+
+            if (j == arr[i].length - 1) {
+              if (adjacentSymbol) {
+                sum = sum + Number(value)
+              }
+            }
+          } else {
+            if (value.length > 0) {
+              if (adjacentSymbol) {
+                sum = sum + Number(value)
+              }
+              value = ""
+              adjacentSymbol = false
+            }
           }
         }
       }
--- 2023/day3/test/main.test.ts
+++ 2023/day3/test/main.test.ts
@@ -2,10 +2,16 @@ import { describe, expect, test } from "bun:test"
 import { Main } from "../src/main"
 
 describe("Main", () => {
-  test("first case", () => {
+  test("part 1 first case", () => {
     expect(new Main().execute({ test: true })).toBe(4361)
   })
-  test("full case", () => {
+  test("part 1 full case", () => {
     expect(new Main().execute()).toBe(539433)
+  })
+  test("part 2 first case", () => {
+    expect(new Main().execute({ test: true, part2: true })).toBe(467835)
+  })
+  test("part 2 full case", () => {
+    expect(new Main().execute({ part2: true })).toBe(75847567)
   })
 })
